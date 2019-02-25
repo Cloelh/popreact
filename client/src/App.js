cfcab668fcbc188ff6a8af72ef34f0f184a2bf80
cloelh <cloelh@outlook.fr>
2019-02-25 14:58:37 +0100

    add

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
 //root
 import Home from "./Home.js";
 import About from "./About.js";
+import Questions from "./questions.js"
 
 
 
@@ -19,12 +20,10 @@ class App extends Component {
                     <header>
 						<div>Potatoes or Pasta?</div>
 					</header>
-					<div>
-
-					</div>
 					<Switch>
                         <Route exact={true} path="/" component={Home} />
                         <Route exact={true} path="/home" component={Home} />
+                        <Route exact={true} path="/questions" component={Questions} />
                         <Route exact={true} path="/about" component={About} />
                         <Route path="*" component={() => <p>Page Not Found</p>} />
                     </Switch>
---
+++ client/src/questions.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+import {quizzes, users} from './examples';
+import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
+
+//root
+
+
+
+class Questions extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Question</h1>
+                <form>
+
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Questions;
