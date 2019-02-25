25309a04bf2faba9575cb81912c2126eca696146
cloelh <cloelh@outlook.fr>
2019-02-25 13:57:15 +0100

    add

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import {quizzes, users} from './examples';
 import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
 
+//root
+import Home from "./Home.js";
+import About from "./About.js";
 
 
 class App extends Component {
-
     render() {
-	return (
-    	  <div>
-	    Hey guys!!
-	  </div>
-	);
+        return (
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route exact={true} path="/" component={Home} />
+                        <Route exact={true} path="/home" component={Home} />
+                        <Route exact={true} path="/about" component={About} />
+                        <Route path="*" component={() => <p>Page Not Found</p>} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
     }
 }
 
